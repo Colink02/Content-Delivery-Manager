801da1e8567e5881c5eeac299ccca3167d6e9850
Colink02 <colinkinzel@gmail.com>
2024-11-02 12:02:21 -0500

    Added header component

---
+++ src/components/FileSystemView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="nav">
+    <div>
+      <a href="/">CDN Manager</a>
+    </div>
+    <div class="nav-right">
+      <a href="#">
+      </a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.nav-right {
+  display: flex;
+  text-align: right;
+}
+.nav {
+  display: flex;
+  width: 100%;
+  background-color: #244a68;
+  height: 55px;
+  align-items: center;
+  min-height: 55px;
+  padding: 5px;
+}
+.icon {
+  height: 25px;
+}
+</style>
