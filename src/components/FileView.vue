56b010f2b2ef1bad7b838a7f5fef01203cc32571
Colink02 <colinkinzel@gmail.com>
2024-11-02 11:54:05 -0500

    Added views for files and folders

--- src/components/FileView.vue
+++ src/components/FileView.vue
@@ -1,11 +1,136 @@
 <script setup lang="ts">
-
+import FileItem from "@/components/FileItem.vue";
 </script>
 
 <template>
-
+  <div class="file-view">
+    <h4>Files</h4>
+    <div class="files">
+    <FileItem
+      file_type_icon="/icons/file-text.svg"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+    <FileItem
+      file_type_icon="https://placehold.co/64"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+    <FileItem
+      file_type_icon="https://placehold.co/64"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+    <FileItem
+      file_type_icon="https://placehold.co/64"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+    <FileItem
+      file_type_icon="https://placehold.co/64"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+    <FileItem
+      file_type_icon="https://placehold.co/64"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+    <FileItem
+      file_type_icon="https://placehold.co/64"
+      file_name="test.txt"
+      preview_image_url="https://placehold.co/600x400"
+    />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+      <FileItem
+        file_type_icon="https://placehold.co/64"
+        file_name="test.txt"
+        preview_image_url="https://placehold.co/600x400"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.file-view {
+  margin: 16px;
+  width: 100%;
+}
+.files {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
+  padding-bottom: 50px;
+}
 </style>
---
+++ src/components/FolderView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import FolderItem from "@/components/FolderItem.vue";
+</script>
+
+<template>
+  <div class="folder-view">
+    <h4>Folders</h4>
+    <div class="folders">
+      <FolderItem
+        folder_icon="/icons/folder.svg"
+        folder_name="Test"
+      ></FolderItem>
+      <FolderItem
+        folder_icon="https://placehold.co/64"
+        folder_name="Test"
+      ></FolderItem>
+      <FolderItem
+        folder_icon="https://placehold.co/64"
+        folder_name="Test"
+      ></FolderItem>
+      <FolderItem
+        folder_icon="https://placehold.co/64"
+        folder_name="Test"
+      ></FolderItem>
+      <FolderItem
+        folder_icon="https://placehold.co/64"
+        folder_name="Test"
+      ></FolderItem>
+      <FolderItem
+        folder_icon="https://placehold.co/64"
+        folder_name="Test"
+      ></FolderItem>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.folders {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.folder-view {
+  margin: 16px;
+}
+</style>
