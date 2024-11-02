5938312a8e39016213e15e13521775b4ad61710f
Colink02 <colinkinzel@gmail.com>
2024-11-02 12:01:51 -0500

    Working on adding Drag and drop functionality similar to Drive

---
+++ src/components/DraggableFileItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps(['file_name', 'file_preview_img', "handleDrag"]);
+
+// TODO make it so you can drag and drop files onto folders and it shows a little thing following your cursor
+
+</script>
+
+<template>
+  <div class="draggable" >
+    <span>{{ file_name }}</span>
+  </div>
+</template>
+
+<style scoped>
+.draggable {
+  width: 100px;
+  height: 100px;
+  background-color: #244a68;
+  border: 1px solid #ccc;
+  cursor: move;
+  position: absolute;
+}
+</style>
