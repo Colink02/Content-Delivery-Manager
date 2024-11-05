d2f4084e40c627076f60daddfd03f0a38fd3ff2f
Colink02 <colinkinzel@gmail.com>
2024-11-05 00:59:04 -0600

    Added a getter: selectedFileNames and clearSelection function

--- src/stores/view_state.ts
+++ src/stores/view_state.ts
@@ -3,13 +3,24 @@ import { defineStore } from "pinia";
 export const useViewState = defineStore("AppState", {
   state: () => ({ showDetails: true, files: [], selectedFiles: [] }),
   getters: {
+    selectedFileNames: (state) => {
+      console.log(typeof state.selectedFiles);
+      console.log(state.selectedFiles);
+      return state.selectedFiles.map(file => file.name).join(" ");
+    }
   },
   actions: {
     toggleDetails() {
       this.showDetails = !this.showDetails;
     },
     addSelectItem(item: string) {
-      this.selectedFiles += item;
+      if(this.selectedFiles.length > 0) {
+        this.selectedFiles = [];
+      }
+      this.selectedFiles.push(item);
+    },
+    clearSelection() {
+      this.selectedFiles = [];
     }
   },
 });
