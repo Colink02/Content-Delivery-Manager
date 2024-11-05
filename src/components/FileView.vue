92a5107f63dac0f5702a428863b9fbf214b19453
Colink02 <colinkinzel@gmail.com>
2024-11-05 00:58:08 -0600

    FileView.vue and FolderView.vue will now pull files from the backend server

--- src/components/FileView.vue
+++ src/components/FileView.vue
@@ -1,121 +1,24 @@
 <script setup lang="ts">
 import FileItem from "@/components/FileItem.vue";
+import { ref } from "vue";
+import { useViewState } from "@/stores/view_state";
+
+const files = ref([]);
+
+fetch("http://localhost:8080/files").then((res) => res.json()).then((data) => {
+  files.value = data.files;
+})
+function removeSelectedItems() {
+  useViewState().clearSelection();
+}
 </script>
 
 <template>
-  <div class="file-view">
+  <div class="file-view" @mouseup="removeSelectedItems">
     <h4>Files</h4>
     <div class="files">
-    <FileItem
-      file_type_icon="/icons/file-text.svg"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-    <FileItem
-      file_type_icon="https://placehold.co/64"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-    <FileItem
-      file_type_icon="https://placehold.co/64"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-    <FileItem
-      file_type_icon="https://placehold.co/64"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-    <FileItem
-      file_type_icon="https://placehold.co/64"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-    <FileItem
-      file_type_icon="https://placehold.co/64"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-    <FileItem
-      file_type_icon="https://placehold.co/64"
-      file_name="test.txt"
-      preview_image_url="https://placehold.co/600x400"
-    />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
-      <FileItem
-        file_type_icon="https://placehold.co/64"
-        file_name="test.txt"
-        preview_image_url="https://placehold.co/600x400"
-      />
+      <file-item :file_name="file.name" file_type_icon="/icons/file.svg" v-for="file in files" :key="file.name">
+      </file-item>
     </div>
   </div>
 </template>
--- src/components/FolderView.vue
+++ src/components/FolderView.vue
@@ -1,35 +1,29 @@
 <script setup lang="ts">
 import FolderItem from "@/components/FolderItem.vue";
+import { ref } from "vue";
+import { useViewState } from "@/stores/view_state";
+
+let folders = ref([]);
+
+fetch("http://localhost:8080/files").then(
+  res => res.json()
+).then(json => {
+  console.log("data recieved: ", json);
+  folders.value = json.folders;
+});
+
+function removeSelectedItems() {
+  useViewState().clearSelection();
+}
+
 </script>
 
 <template>
-  <div class="folder-view">
+  <div class="folder-view" @mouseup="removeSelectedItems">
     <h4>Folders</h4>
     <div class="folders">
-      <FolderItem
-        folder_icon="/icons/folder.svg"
-        folder_name="Test"
-      ></FolderItem>
-      <FolderItem
-        folder_icon="https://placehold.co/64"
-        folder_name="Test"
-      ></FolderItem>
-      <FolderItem
-        folder_icon="https://placehold.co/64"
-        folder_name="Test"
-      ></FolderItem>
-      <FolderItem
-        folder_icon="https://placehold.co/64"
-        folder_name="Test"
-      ></FolderItem>
-      <FolderItem
-        folder_icon="https://placehold.co/64"
-        folder_name="Test"
-      ></FolderItem>
-      <FolderItem
-        folder_icon="https://placehold.co/64"
-        folder_name="Test"
-      ></FolderItem>
+      <folder-item :folder_name="folder.name" folder_icon="/icons/folder.svg"  v-for="folder in folders" :key="folder.name">
+      </folder-item>
     </div>
   </div>
 </template>
