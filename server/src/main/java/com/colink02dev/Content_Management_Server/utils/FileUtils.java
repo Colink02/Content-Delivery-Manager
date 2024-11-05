b8201c30888a4ee6235594ad3763e6f4802cc78c
Colink02 <colinkinzel@gmail.com>
2024-11-05 00:57:27 -0600

    FileUtils methods are now static and will return files and folders as well as check for special folder types to be handled in the future

--- server/src/main/java/com/colink02dev/Content_Management_Server/utils/FileUtils.java
+++ server/src/main/java/com/colink02dev/Content_Management_Server/utils/FileUtils.java
@@ -8,22 +8,28 @@ import java.io.File;
 
 public class FileUtils {
 
-public JsonObject getFiles(File[] files) {
+public static JsonObject getFiles(File[] files) {
     JsonObject obj = new JsonObject();
-    JsonArray folders = new JsonArray();
+    JsonArray folderData = new JsonArray();
+    JsonArray fileData = new JsonArray();
     for (File file : files) {
         if (file.isDirectory()) {
             JsonObject folderInfo = new JsonObject();
             folderInfo.addProperty("name", file.getName());
             folderInfo.add("special_types", checkForSpecialFolderTypes(file));
-            folders.add(folderInfo);
+            folderData.add(folderInfo);
+        } else {
+            JsonObject fileInfo = new JsonObject();
+            fileInfo.addProperty("name", file.getName());
+            fileData.add(fileInfo);
         }
     }
-    obj.add("folders", folders);
+    obj.add("folders", folderData);
+    obj.add("files", fileData);
     return obj;
 }
 
-private JsonArray checkForSpecialFolderTypes(File folder) {
+private static JsonArray checkForSpecialFolderTypes(File folder) {
     JsonArray specialTypes = new JsonArray();
     for(FolderTypes type: FolderTypes.values()) {
         if(type.getHandler().isType(folder)) {
